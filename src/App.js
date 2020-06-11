e348673e279867a825cc000a98a64a28b57b9fa3
haileyhansard <haileyhansard@gmail.com>
2020-06-10 18:32:00 -0700

    Search Bar and PostsPage are working correctly

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
-// import the PostsPage and SearchBar and add them to the App
-import PostsPage from "./components/PostsContainer";
-import SearchBar from "./components";
+//import the PostsPage and SearchBar and add them to the App
+
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import PostsPage from './components/PostsContainer/PostsPage';
 
 const App = () => {
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      {/* Add imported components here to render them */ }   
+      <SearchBar />
+      <PostsPage />
     </div>
   );
 };
